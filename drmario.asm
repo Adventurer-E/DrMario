5d16cdf583dde6414d601954faf7a61ffae7618a
Adventurer-E <erickudou@gmail.com>
2024-11-19 12:27:46 -0500

    some changes

--- drmario.asm
+++ drmario.asm
@@ -45,6 +45,8 @@ MIDDLERIGHT: # (34,3)
     .word 0x1000808f
 BOTTOMLEFT: # (64,1), 0x10008000 + 63 * (64 * 4)
     .word 0x1000bf00
+LOWERHALF: # (1,34), 0x10008000 + 33 * (64 * 4)
+    .word 0x1000a100
 RED:
     .word 0xff0000
 BLUE:
@@ -107,6 +109,42 @@ Array: # array to store information of capsules
     addi $a1, $zero, 63
     add $t0, $zero, $zero
     jal draw_horizontal_line
+
+    ############################# Set up viruses ################################
+ # # Currently, 4 viruses.
+    # addi $t2, $zero, 4 # in this section (before making capsules), t2 stores the number of viruses
+    # add $t3, $zero, $zero # t3 stores current number of visuses (accumulator)
+# new_virus:
+    # # y-coord of random location
+    # li $v0, 42
+    # li $a0, 0
+    # li $a1, 30
+    # syscall # stored at a0
+    # subi $a0, $a0, 1 # first row inclusive
+    # sll $t4, $a0, 128 # times 256, temp stored in $t4
+    # lw $t0, LOWERHALF
+    # add $t0, $t0, $t4 # vertically moved
+    # # x-coord of random location
+    # li $v0, 42
+    # li $a0, 0
+    # li $a1, 62
+    # syscall # stored at a0
+    # sll $a0, $a0, 2 # times 4
+    # add $t0, $t0, $a0 # horizontally moved
+    # # random color
+    # li $v0, 42
+    # li $a0, 0
+    # li $a1, 3
+    # syscall
+    # add $t1, $zero, $a0
+    # jal determine_color_1
+    # # paint
+    # sw $t1, 0($t0)
+    # # increment
+    # addi $t3, $t3, 1
+    # beq $t3, $t2, virus_end
+    # j new_virus
+# virus_end:
 
     ############################# First capsule ################################
 main:    
